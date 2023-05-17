1b15b0fadc1a69de19aa55d127194a0e41bac424
Hugo Gomes <hugogomes843@hotmail.com>
2023-05-17 15:46:02 +0100

    segundo exercicio completo

--- Form3.Designer.vb
+++ Form3.Designer.vb
@@ -22,9 +22,125 @@ Partial Class Form3
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form3"
+        Panel1 = New Panel()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        Button1 = New Button()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Panel1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Controls.Add(Label4)
+        Panel1.Controls.Add(Label3)
+        Panel1.Controls.Add(Label2)
+        Panel1.Controls.Add(Button1)
+        Panel1.Controls.Add(TextBox4)
+        Panel1.Controls.Add(TextBox3)
+        Panel1.Controls.Add(TextBox2)
+        Panel1.Controls.Add(TextBox1)
+        Panel1.Controls.Add(Label1)
+        Panel1.Location = New Point(20, 14)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(758, 418)
+        Panel1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(62, 37)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(170, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Calcular Volume de um tanque"' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(194, 113)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(282, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(194, 175)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(282, 23)
+        TextBox2.TabIndex = 2
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(194, 236)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(282, 23)
+        TextBox3.TabIndex = 3
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(240, 335)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(236, 23)
+        TextBox4.TabIndex = 4
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(93, 317)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(113, 57)
+        Button1.TabIndex = 5
+        Button1.Text = "Calcular Volume"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(106, 116)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(82, 15)
+        Label2.TabIndex = 6
+        Label2.Text = "Comprimento"' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(125, 178)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(47, 15)
+        Label3.TabIndex = 7
+        Label3.Text = "Largura"' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(133, 244)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(39, 15)
+        Label4.TabIndex = 8
+        Label4.Text = "Altura"' 
+        ' Form3
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Panel1)
+        Name = "Form3"
+        Text = "Form3"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        ResumeLayout(False)
     End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
 End Class
--- Form3.vb
+++ Form3.vb
@@ -1,3 +1,11 @@
 ﻿Public Class Form3
 
+    Dim aux As String
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.TextLength <> 0 And TextBox2.TextLength <> 0 And TextBox3.TextLength <> 0 Then
+            aux = TextBox1.Text * TextBox2.Text * TextBox3.Text
+            TextBox4.Text = aux
+        End If
+    End Sub
 End Class
